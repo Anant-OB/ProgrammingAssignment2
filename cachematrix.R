5ec3264829313ffc79939c0071f3dbe8ad83c693
Anant-OB <anantonkarbhasin123123@gmail.com>
2020-09-16 03:15:06 +0530

    I have written a function to save computational time.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function just like the example one taken a matrix argument and tries to load the matrix and sets the values for its inverse such that the getinverse function 
+## can return the cache of inverse matrix. The variable are similar to the mean example present just before this.
 
-## Write a short comment describing this function
+## This function have been tested for multiple matrices and found to be reliable. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL						## the value of inverse will be stored in this object only throughout the function
+  set <- function(y) {				## takes the matrix and sets it.
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x				## Stores the matrix
+  si <- function(solve) {m <<- solve}		
+  gi <- function() {m}
+  list(set=set,get=get,si=si,gi=gi)		## This list contains all the values.
 }
 
 
-## Write a short comment describing this function
+## The function below tries to check if their is a cache copy of the inverse of the matrix if not then it tries to calculate the inverse of the matrix 
+## and sets it to the cache function in setinverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$gi()
+  if(!is.null(m)) {
+    message("Already calculated here you go!")
+    return(m)				##IMP: Don't use print() funtion in this place return() is perfect here.
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$si(m)
+  m
 }
